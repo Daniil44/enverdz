296fe0e47af79429f10ed2daed56ded4f3faff88
xcclyan <daniilefimenko287@gmail.com>
2026-06-24 15:05:04 +0300

    Add files via upload

---
+++ Новая папка 10/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace WpfApp1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+
+        dialog.Filter = "Text files (*.txt)|*.txt";
+
+
+        if (dialog.ShowDialog() == true)
+        {
+            string text = File.ReadAllText(dialog.FileName);
+
+            textBox1.Text = text;
+        }
+    }
+}
