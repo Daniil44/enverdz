91e1bc0bc4fc9d26c2aa4e568c49c4ae27ac6343
xcclyan <daniilefimenko287@gmail.com>
2026-06-24 15:01:00 +0300

    Add files via upload

---
+++ Новая папка 8/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace WpfApp1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Rectangle r = new Rectangle();
+
+        r.Width = 100;
+        r.Height = 100;
+        r.Fill = Brushes.Red;
+
+        canvas1.Children.Add(r);
+    }
+}
